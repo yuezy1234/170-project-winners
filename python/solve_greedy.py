0e157f1c2a74b537012a424cfeb82b3c0babead9
yuezy1234 <yuezy2015@gmail.com>
2022-04-25 17:03:57 -0700

    Added donuts

--- python/solve_greedy.py
+++ python/solve_greedy.py
@@ -15,6 +15,14 @@ def squares_in_range(x, y, radius, D):
                 squares.append([xp, yp])
     return squares
 
+def donut_in_range(x, y, inner, outer, D):
+    squares = []
+    for xp in range(x-outer, x+outer+1):
+        for yp in range(y-outer, y+outer+1):
+            if xp >= 0 and yp >= 0 and xp < D and yp < D and (x - xp)**2 + (y - yp)**2 <= outer**2 and (x - xp)**2 + (y - yp)**2 > inner**2:
+                squares.append([xp, yp])
+    return squares
+
 def greedy_solver(instance: Instance) -> Solution:
 
     D = instance.D
@@ -26,6 +34,7 @@ def greedy_solver(instance: Instance) -> Solution:
     towers_map = np.zeros((D, D)) # Boolean of whether there is a tower there
     cities_in_range = np.zeros((D, D)) # Tracks the number of cities in range on each square
     towers_in_range = np.zeros((D, D)) # Tracks the penalty on each square
+    donuts_in_range = np.zeros((D, D))
     # rating_tracker = np.array((D, D)) # Tracks the rating for each square, puts a tower at the maximum one (greedy)
 
     city_map = np.zeros((D, D))
@@ -36,9 +45,12 @@ def greedy_solver(instance: Instance) -> Solution:
         city_map[x][y] += 1
         for square in squares_in_range(x, y, Rs, D):
             cities_in_range[square[0]][square[1]] += 1
+        for square in donut_in_range(x, y, Rs, Rp + Rs, D):
+            donuts_in_range[square[0]][square[1]] += 1
 
-    city_award = 2
-    tower_penalty = -1
+    city_award = 5
+    tower_penalty = -3
+    donut_penalty = -4
 
     while cities_left > 0:
         best_rating_square = [0, 0]
@@ -46,7 +58,9 @@ def greedy_solver(instance: Instance) -> Solution:
         for x in range(D):
             for y in range(D):
                 if towers_map[x][y] == 0 and cities_in_range[x][y] > 0:
-                    current_rating = city_award * cities_in_range[x][y] + tower_penalty * towers_in_range[x][y]
+                    current_rating = city_award * cities_in_range[x][y] 
+                    + tower_penalty * towers_in_range[x][y]
+                    + donut_penalty * donuts_in_range[x][y]
                     if current_rating > best_rating:
                         best_rating_square = [x, y]
                         best_rating = current_rating
@@ -62,8 +76,12 @@ def greedy_solver(instance: Instance) -> Solution:
                 cities_left -= 1
                 for city_neighbor in squares_in_range(city_x, city_y, Rs, D):
                     cities_in_range[city_neighbor[0]][city_neighbor[1]] -= 1
+                for city_neighbor in donut_in_range(city_x, city_y, Rs, Rp + Rs, D):
+                    donuts_in_range[city_neighbor[0]][city_neighbor[1]] -= 1
         for square in squares_in_range(x, y, Rp, D):
             towers_in_range[square[0]][square[1]] += 1
+        
+        print(f"built tower at {x}, {y} with a score of {best_rating}")
 
     sol = Solution(instance=instance,
                         towers=[Point(x=i,y=j) for i in range(D) for j in range(D) if towers_map[i][j]>0])
