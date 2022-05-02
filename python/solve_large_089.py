751597a14175f686380e03a6cf0558c8097df655
yuezy1234 <yuezy2015@gmail.com>
2022-05-02 01:04:48 -0700

    Ran more randomized greedy on medium for the afternoon. Stored in jonathan_improvements_1 and 2.

---
+++ python/solve_large_089.py
@@ -0,0 +1,9 @@
+from point import Point
+from instance import Instance
+from solution import Solution
+
+def greedy_solver_savestates(instance: Instance) -> Solution:
+    coords = []
+    for i in range(0, 5):
+        
+
