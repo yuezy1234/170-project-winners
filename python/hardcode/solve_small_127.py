e349ed9af8d4bc98affe4fb4abe856468545f3c0
Gavin Yu <gavinyu2003@berkeley.edu>
2022-05-02 03:04:54 -0700

    solved small 127 by hand

--- python/hardcode/solve_small_127.py
+++ python/hardcode/solve_small_127.py
@@ -4,17 +4,18 @@ from solution import Solution
 
 def small_127(instance: Instance) -> Solution:
     towers = [
-        [15, 16],
+        [1, 7],
+        [1, 21],
+        [6, 14],
+        [6, 25],
+        [13, 18],
         [15, 0],
-        [3, 12],
-        [27, 12],
-        [3, 17],
-        [27, 17],
-        [10, 21],
-        [20, 21],
-        [3, 22],
-        [27, 22],
-        [11, 29],
-        [19, 29]
+        [15, 10],
+        [15, 27],
+        [17, 18],
+        [24, 25],
+        [24, 14],
+        [29, 21],
+        [29, 7]
     ]
     return Solution(instance=instance, towers=[Point(x = tower[0], y = tower[1]) for tower in towers])
